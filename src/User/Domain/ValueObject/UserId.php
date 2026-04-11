5e09028c39f305acb6dcf96b1713ab57a221cd49
LEmanRUS17 <lemanrus17@gmail.com>
2026-04-11 23:47:19 +0500

    validate uuid

--- src/User/Domain/ValueObject/UserId.php
+++ src/User/Domain/ValueObject/UserId.php
@@ -34,6 +34,10 @@ final class UserId
 
     public static function fromString(string $value): self
     {
+        if (!preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i', $value)) {
+            throw new \InvalidArgumentException("Invalid UUID v4: \"{$value}\"");
+        }
+
         return new self($value);
     }
 }
