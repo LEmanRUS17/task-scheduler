c4d347f162c3c0e762dbfd128f5d579ec5b53a39
LEmanRUS17 <lemanrus17@gmail.com>
2026-04-14 14:57:41 +0500

    update migrations

--- migrations/Version20260410141649.php
+++ migrations/Version20260410141649.php
@@ -7,20 +7,29 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20260410141649 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Create user table';
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE "user" (id VARCHAR(36) NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, status VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, password_updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, roles JSON NOT NULL, PRIMARY KEY (id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
+        $this->addSql(
+            'CREATE TABLE "user" (id VARCHAR(36) NOT NULL, 
+            email VARCHAR(180) NOT NULL, 
+            password VARCHAR(255) NOT NULL, 
+            status VARCHAR(255) NOT NULL, 
+            created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, 
+            deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, 
+            password_updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, 
+            roles JSON NOT NULL, PRIMARY KEY (id))'
+        );
+        $this->addSql(
+            'CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 
+            ON "user" (email)'
+        );
     }
 
     public function down(Schema $schema): void
--- migrations/Version20260413182556.php
+++ migrations/Version20260413182556.php
@@ -7,25 +7,37 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20260413182556 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Create profile table with foreign key to user';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE profile (user_id VARCHAR(36) NOT NULL, username VARCHAR(255) DEFAULT NULL, firstname VARCHAR(255) DEFAULT NULL, lastname VARCHAR(255) DEFAULT NULL, midlname VARCHAR(255) DEFAULT NULL, status VARCHAR(255) DEFAULT NULL, last_login TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY (user_id))');
+        $this->addSql(
+            'CREATE TABLE profile (
+            user_id VARCHAR(36) NOT NULL, 
+            username VARCHAR(255) DEFAULT NULL, 
+            firstname VARCHAR(255) DEFAULT NULL, 
+            lastname VARCHAR(255) DEFAULT NULL, 
+            midlname VARCHAR(255) DEFAULT NULL, 
+            status VARCHAR(255) DEFAULT NULL, 
+            last_login TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, 
+            created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, 
+            PRIMARY KEY (user_id))'
+        );
+        $this->addSql(
+            'ALTER TABLE profile ADD CONSTRAINT FK_8157AA0FA76ED395 
+            FOREIGN KEY (user_id) REFERENCES "user" (id) 
+            NOT DEFERRABLE INITIALLY IMMEDIATE'
+        );
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE profile DROP CONSTRAINT FK_8157AA0FA76ED395');
         $this->addSql('DROP TABLE profile');
     }
 }
