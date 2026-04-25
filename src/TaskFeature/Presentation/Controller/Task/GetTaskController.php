14f48a76580c4e8c5a39ac8053139fe1dae26d52
LEmanRUS17 <lemanrus17@gmail.com>
2026-04-25 18:49:48 +0500

    get task by id

---
+++ src/TaskFeature/Presentation/Controller/Task/GetTaskController.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\TaskFeature\Presentation\Controller\Task;
+
+use App\TaskFeatureApi\Service\TaskServiceInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[AsController]
+final class GetTaskController
+{
+    public function __construct(
+        private readonly TaskServiceInterface $taskService,
+    ) {}
+
+    #[Route('/task/{id}', name: 'task_get', methods: ['GET'])]
+    public function __invoke(string $id): JsonResponse
+    {
+        $task = $this->taskService->getById($id);
+
+        if ($task === null) {
+
+            return new JsonResponse(
+                [
+                    'success' => false,
+                    'message' => 'Task not found'
+                ],
+                Response::HTTP_NOT_FOUND,
+            );
+        }
+
+        return new JsonResponse(
+            [
+                'id' => $task->getId(),
+                'title' => $task->getTitle(),
+                'status' => $task->getStatus(),
+                'createdAt' => $task->getCreatedAt()->format(\DateTimeInterface::ATOM),
+            ],
+            Response::HTTP_OK,
+        );
+    }
+}
