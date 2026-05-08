a75a5e3879e23407791b9aabcef8744c6c181166
LEmanRUS17 <lemanrus17@gmail.com>
2026-05-08 21:16:16 +0500

    fix: send email. issue #14

--- src/NotificationFeature/Infrastructure/Messenger/Handler/TaskStatusChangedHandler.php
+++ src/NotificationFeature/Infrastructure/Messenger/Handler/TaskStatusChangedHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\NotificationFeature\Infrastructure\Messenger\Handler;
 
+use App\SubscriptionFeature\Domain\ValueObject\NotificationChannel;
 use App\SubscriptionFeatureApi\Service\SubscriptionServiceInterface;
 use App\TaskFeature\Infrastructure\Messenger\Message\TaskStatusChangedMessage;
 use App\TaskFeatureApi\Service\TaskServiceInterface;
@@ -45,7 +46,7 @@ final class TaskStatusChangedHandler
 
             foreach ($subscription->getChannels() as $channel) {
                 match ($channel) {
-                    'email' => $this->sendEmail(
+                    NotificationChannel::EMAIL->value => $this->sendEmail(
                         $user->getEmail(),
                         $task->getTitle(),
                         $message->fromStatus,
