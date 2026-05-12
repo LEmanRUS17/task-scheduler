184e587be55dafb64e9fa7f6ff4e9dc6ace61278
LEmanRUS17 <lemanrus17@gmail.com>
2026-05-12 17:51:22 +0500

    feat: loading a task into manticore. issue #22

---
+++ src/SearchFeature/Infrastructure/Messenger/EventSubscriber/TaskSearchIndexSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\SearchFeature\Infrastructure\Messenger\EventSubscriber;
+
+use App\SearchFeature\Infrastructure\Messenger\Message\IndexTaskMessage;
+use App\TaskFeature\Domain\Event\TaskCreated;
+use App\TaskFeature\Domain\Event\TaskStatusChanged;
+use App\TaskFeature\Domain\Event\TaskUpdated;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class TaskSearchIndexSubscriber
+{
+    public function __construct(private readonly MessageBusInterface $defaultBus) {}
+
+    #[AsMessageHandler(bus: 'event.bus')]
+    public function onTaskCreated(TaskCreated $event): void
+    {
+        $this->defaultBus->dispatch(new IndexTaskMessage($event->id->value()));
+    }
+
+    #[AsMessageHandler(bus: 'event.bus')]
+    public function onTaskUpdated(TaskUpdated $event): void
+    {
+        $this->defaultBus->dispatch(new IndexTaskMessage($event->id->value()));
+    }
+
+    #[AsMessageHandler(bus: 'event.bus')]
+    public function onTaskStatusChanged(TaskStatusChanged $event): void
+    {
+        $this->defaultBus->dispatch(new IndexTaskMessage($event->taskId));
+    }
+}
